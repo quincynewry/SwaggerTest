6e8232cc8fa21dac16583f36677bebbf9cdd5100
Mark Quinn <markquinn06@yahoo.co.uk>
2018-04-03 17:27:39 +0100

    initial commit

---
+++ SwaggerTest/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SwaggerTest.Models;
+
+namespace SwaggerTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+
+        /// <summary>
+        /// Gets some model.
+        /// </summary>
+        [HttpGet]
+        [ProducesResponseType(typeof(SomeModel), 200)]
+        public SomeModel Get()
+        {
+            return new SomeModel { MyProperty1 = 1, MyProperty2 = "Test" };
+        }
+    }
+}
---
+++ SwaggerTest/Models/SomeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SwaggerTest.Models
+{
+    public class SomeModel
+    {
+        public int MyProperty1 { get; set; }
+
+        public string MyProperty2 { get; set; }
+    }
+}
